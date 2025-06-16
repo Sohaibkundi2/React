79368d77f04a1ae9b19f1581f5bbfc5034876469
Sohaibkundi2 <sohaibkundi2@gmail.com>
2025-06-17 00:26:12 +0500

    Update layout structure and styles for container and box components in background changer app

--- istProject/src/App.jsx
+++ istProject/src/App.jsx
@@ -9,16 +9,18 @@ export default function App() {
 
 
 
-  const [color, setcolor] = useState('yellow');
+  const [color, setcolor] = useState('aquamarine');
   return (
-      <div className="box" style={{backgroundColor:color}} >
-        <button onClick={()=>{setcolor("red")}} style={{backgroundColor:'red'}} className="btn">Red</button>
-        <button onClick={()=>{setcolor("green")}} style={{backgroundColor:'green'}} className="btn">Green</button>
-        <button onClick={()=>{setcolor("yellow")}} style={{backgroundColor:'yellow'}} className="btn">Yellow</button>
+      <div className="container" style={{backgroundColor:color}} >
+       <div className="box">
+         <button onClick={()=>setcolor("red")} style={{backgroundColor:'red'}} className="btn">Red</button>
+        <button onClick={()=>setcolor("green")} style={{backgroundColor:'green'}} className="btn">Green</button>
+        <button onClick={()=>setcolor("yellow")} style={{backgroundColor:'yellow'}} className="btn">Yellow</button>
         <button onClick={()=>{setcolor("blue")}} style={{backgroundColor:'blue'}} className="btn">Blue</button>
         <button onClick={()=>{setcolor("orangered")}} style={{backgroundColor:'orangered'}} className="btn">Orangered</button>
         <button onClick={()=>{setcolor("white")}} style={{backgroundColor:'white'}} className="btn">White</button>
         <button onClick={()=>{setcolor("purple")}} style={{backgroundColor:'purple'}} className="btn">Purple</button>
+       </div>
       </div>
   )
 }
